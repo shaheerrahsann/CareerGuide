42da520cd1ea87f6f2b2fd8cfa1f3cf3d2158fae
shaheerrahsann <89391833+shaheerrahsann@users.noreply.github.com>
2021-08-26 15:31:32 +0500

    Add files via upload

---
+++ typed.js
@@ -0,0 +1,14 @@
+var app = document.getElementById("app1");
+// 2. Create a new <p></p> element programmatically
+var p1 = document.createElement("p1");
+var p = document.createElement("p");
+// 3. Add the text content
+//p.textContent = "Hello    , World!";
+var para1 = document.getElementById("para1");
+// 4. Append the p element to the div element
+p1.textContent = "hello ";
+app.appendChild(p1);
+p.textContent = "blank ";
+para1.appendChild(p);
+var p12 = document.getElementById("para12");
+p12.textContent = "hi!";
